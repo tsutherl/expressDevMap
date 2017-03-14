0e6f059d76eff446fe0faee3b2aa331047c9981c
ScoutOK <ohkeefe@gmail.com>
2017-03-14 16:02:38 -0400

    added class to modal div

--- actual-app/src/TestModal.js
+++ actual-app/src/TestModal.js
@@ -17,7 +17,7 @@ export default class TestModal extends React.Component {
 		const route = this.props.testRoute;
 		console.log("props in testModal ", this.props);
 			return (
-		<div>
+		<div className='modal'>
 			<p> I'm just a boring modal for now </p>
 			<p> if you click <button onClick={()=>this.handleClick(route)}>me</button> you can test this route: </p>
 			<p> {this.props.testRoute} </p>
