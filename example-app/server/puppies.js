23ec1f0041f4af121f8dd322865aab3ccc036381
Tatiana <sutherland.tati@gmail.com>
2017-03-13 21:23:15 -0400

    object including verbs

--- example-app/server/puppies.js
+++ example-app/server/puppies.js
@@ -1,13 +1,9 @@
 module.exports = require('express').Router()
 
-    .get('/', (req,res) => {
-          res.send(['Henry', 'Boomer', 'Nugget', 'Max']);
-    })
+    .get('/', (req,res) => res.send(['Henry', 'Boomer', 'Nugget', 'Max']))
 
-    .get('/Boomer', (req, res) => {
-        res.send('Boomer');
-    })
+    .get('/Boomer', (req, res) => res.send('Boomer'))
 
-    .get('/Nugget', (req, res) =>{
-        res.send('Nugget')
-    })
+    .get('/Nugget', (req, res) => res.send('Nugget'))
+
+    .post('/', (req, res) => res.send('NEW PUP SUCCESSFULLY CREATED', req.body))
