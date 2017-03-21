a52fa5d19d9349e688e7c0473bc6878ef00a3382
Tatiana <sutherland.tati@gmail.com>
2017-03-21 15:00:42 -0400

    forgot to delete the old store

--- actual-app/assets/app.js
+++ actual-app/assets/app.js
@@ -5731,63 +5731,10 @@ module.exports = setInnerHTML;
 
 /***/ }),
 /* 44 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
 "use strict";
-
-
-// /*---------------CONSTANTS-----------------*/
-
-
-// const RECEIVE_TEST_RESPONSE = 'RECEIVE_TEST_RESPONSE';
-
-
-// /*---------------ACTION CREATORS-----------------*/
-
-
-// export const routeTestResponse = (result) => ({
-//     type: RECEIVE_TEST_RESPONSE,
-//     result
-// })
-
-// export const setRouteVerb = (verb) => ({
-//     type: SET_TEST_VERB, 
-//     verb
-// })
-
-
-// /*---------------REDUCER-----------------*/
-
-// const reducer = (state={showModal: false, activeTestNode: null, testRoute: null}, action) => {
-//     const newState = Object.assign({}, state)
-//     switch(action.type) {
-//         case RECEIVE_ROUTES:
-//             newState.routes = action.routes;
-//             break
-//         case RECEIVE_TEST_ROUTE:
-//             newState.testRoute = action.testRoute;
-//             break;
-//         case SET_TEST_NODE:
-//             newState.activeTestNode = action.node;
-//             break;
-//         case SHOW_MODAL:
-//             newState.showModal = true;
-//             break;
-//         case HIDE_MODAL:
-//             newState.showModal = false;
-//             break;
-//         case RECEIVE_TEST_RESPONSE:
-//             newState.testResult = action.result;
-//             break;
-//         case SET_TEST_VERB: 
-//             newState.selectedRouteVerb = action.verb;
-//             break;
-//         default:
-//             return state;
-//     }
-//     return newState
-// }
-
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/Users/tatianasutherland/coding-stuffs/portfolio/backend-tree/actual-app/src/store.js'");
 
 /***/ }),
 /* 45 */
