a91cbd65b7fe1084eb16b344f43b38c868ba525d
Tatiana <sutherland.tati@gmail.com>
2017-04-03 14:18:27 -0400

    adds eslint

---
+++ actual-app/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "semi": ["error", "always"],
+        "space-before-function-paren": ["error", "always"],
+        "arrow-spacing": ["error", { "before": false, "after": false }],
+        "prop=types": [0]
+    },
+};
